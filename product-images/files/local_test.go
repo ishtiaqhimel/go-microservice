a3d563c3c11628c312b53a76633092e6fc7c62f5
Md. Ishtiaq Islam <ishtiaq@appscode.com>
2024-08-13 23:39:49 +0600

    Add product image upload

---
+++ product-images/files/local_test.go
@@ -0,0 +1,66 @@
+package files
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func setupLocal(t *testing.T) (*Local, string, func()) {
+	// create a temporary directory
+	dir := os.TempDir()
+
+	l, err := NewLocal(dir, 1024*1000*5)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return l, dir, func() {
+		// cleanup function
+		//os.RemoveAll(dir)
+	}
+}
+
+func TestSavesContentsOfReader(t *testing.T) {
+	savePath := "/1/test.png"
+	fileContents := "Hello World"
+	l, dir, cleanup := setupLocal(t)
+	defer cleanup()
+
+	err := l.Save(savePath, bytes.NewBuffer([]byte(fileContents)))
+	assert.NoError(t, err)
+
+	// check the file has been correctly written
+	f, err := os.Open(filepath.Join(dir, savePath))
+	assert.NoError(t, err)
+
+	// check the contents of the file
+	d, err := io.ReadAll(f)
+	assert.NoError(t, err)
+	assert.Equal(t, fileContents, string(d))
+}
+
+func TestGetsContentsAndWritesToWriter(t *testing.T) {
+	savePath := "/1/test.png"
+	fileContents := "Hello World"
+	l, _, cleanup := setupLocal(t)
+	defer cleanup()
+
+	// Save a file
+	err := l.Save(savePath, bytes.NewBuffer([]byte(fileContents)))
+	assert.NoError(t, err)
+
+	// Read the file back
+	r, err := l.Get(savePath)
+	assert.NoError(t, err)
+	defer r.Close()
+
+	// read the full contents of the reader
+	d, err := io.ReadAll(r)
+	assert.NoError(t, err)
+	assert.Equal(t, fileContents, string(d))
+}
